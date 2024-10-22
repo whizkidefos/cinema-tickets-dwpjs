932be15812ed4ef2e7d17dd6bd619253f22b09c4
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-10-22 09:15:07 +0100

    Fixed a typo, minor css changes

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-import { TicketService } from './src/service/TicketService.js';
-import { TicketTypeRequest } from './src/domain/TicketTypeRequest.js';
-import TicketPaymentService from './src/thirdparty/paymentgateway/TicketPaymentService.js';
-import SeatReservationService from './src/thirdparty/seatbooking/SeatReservationService.js';
+import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
 import 'dotenv/config';
-
 import pageRoutes from './routes/pageRoutes.js';
 import apiRoutes from './routes/apiRoutes.js';
 
-const paymentService = new TicketPaymentService();
-const reservationService = new SeatReservationService();
-const ticketService = new TicketService(paymentService, reservationService);
-
 
 // Exprees & EJS Setup
-import express from 'express';
-import expressLayouts from 'express-ejs-layouts';
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -42,9 +32,6 @@ app.use('/', pageRoutes);
 app.use('/available-tickets', pageRoutes);
 app.use('/order-history', pageRoutes);
 app.use('/', apiRoutes);
-
-// Handle tickets booking
-app
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
